ed234e6289bcd42bdcf8f704723f3e464da7e80c
Jesus <chumchum239@gmail.com>
2022-10-23 20:49:47 -0400

    api test

--- src/App.js
+++ src/App.js
@@ -1,47 +1,51 @@
-import './App.css';
-import {useState,useEffect} from 'react'
-import axios from 'axios'
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
   //creating IP state
-  const [ip, setIP] = useState('');
-  const [mapUrl, setMapUrl] = useState('');
-  const [lat, setLat] = useState('');
-  const [long, setLong] = useState('');
-  const [info, setInfo] = useState('');
+  const [ip, setIP] = useState("");
+  const [mapUrl, setMapUrl] = useState("");
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
+  const [info, setInfo] = useState("");
 
   //creating function to load ip address from the API
   const getData = async () => {
-    const res = await axios.get('https://geolocation-db.com/json/',{ crossDomain: true })
+    const res = await axios.get("https://ip.seeip.org/json", {
+      crossDomain: true,
+    });
     console.log(res.data);
-    setInfo(res.data);
-    setIP(res.data.IPv4);
+    // setInfo(res.data);
+    setIP(res.data.ip);
+  };
 
-  }
-  
-  useEffect( () => {
+  useEffect(() => {
     //passing getData method to the lifecycle method
-    getData()
-
-  }, [])
+    getData();
+  }, []);
 
   if (!ip) {
     return (
       <div className="App">
-          <h1>Turn off adblock and reload.</h1>
-    </div>
-)
+        <h1>Turn off adblock and reload.</h1>
+      </div>
+    );
   } else {
     return (
       <div className="App">
-        <img width="250" height="150" src={('https://c.tenor.com/3tcJqs1Xpg0AAAAC/hack-hacker.gif')} alt="loading..." />
-        
+        <img
+          width="250"
+          height="150"
+          src={"https://c.tenor.com/3tcJqs1Xpg0AAAAC/hack-hacker.gif"}
+          alt="loading..."
+        />
+
         <h4>IP Address: {ip}</h4>
-        <h4>Location : {info.city}, {info.state} {info.postal}</h4>
-       </div>
+        {/* <h4>Location : {info.city}, {info.state} {info.postal}</h4> */}
+      </div>
     );
   }
-
 }
 
 export default App;

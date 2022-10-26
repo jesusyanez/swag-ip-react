a368d6262a70a787bcb9884e83af0820147d2e23
Jesus <chumchum239@gmail.com>
2022-10-25 23:20:58 -0400

    removed image, changed format

--- src/App.js
+++ src/App.js
@@ -4,48 +4,53 @@ import axios from "axios";
 
 function App() {
   //creating IP state
-  const [ip, setIP] = useState("");
-  const [mapUrl, setMapUrl] = useState("");
-  const [lat, setLat] = useState("");
-  const [long, setLong] = useState("");
+  // const [ip, setIP] = useState("");
+  // const [mapUrl, setMapUrl] = useState("");
+  // const [lat, setLat] = useState("");
   const [info, setInfo] = useState("");
 
   //creating function to load ip address from the API
   const getData = async () => {
-    const res = await axios.get("https://info.aesap.workers.dev", {
+    const res = await axios.get("https://client.aesap.workers.dev", {
       crossDomain: true,
     });
     console.log(res.data);
-    // setInfo(res.data);
-    setIP(res.data.ip);
+    setInfo(res.data);
+    // setIP(res.data.ip);
   };
 
   useEffect(() => {
-    //passing getData method to the lifecycle method
     getData();
   }, []);
 
-  if (!ip) {
-    return (
-      <div className="App">
-        <h1>Turn off adblock and reload.</h1>
+  return (
+    <div className="App">
+      <header>
+        <nav class="navbar bg-grey">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="/">
+              <h1>IP Buddy</h1>
+            </a>
+          </div>
+        </nav>
+      </header>
+      <div className="card">
+        <div class="card-header">
+          <h4>Your IP Data</h4>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">IP Address: {info.ip}</li>
+          <li class="list-group-item">
+            Location : {info.city}, {info.region} {info.postalCode},{" "}
+            {info.country}
+          </li>
+          <li class="list-group-item">
+            Coordinates: {info.latitude}, {info.longitude}
+          </li>
+        </ul>
       </div>
-    );
-  } else {
-    return (
-      <div className="App">
-        <img
-          width="250"
-          height="150"
-          src={"https://c.tenor.com/3tcJqs1Xpg0AAAAC/hack-hacker.gif"}
-          alt="loading..."
-        />
-
-        <h4>IP Address: {ip}</h4>
-        {/* <h4>Location : {info.city}, {info.state} {info.postal}</h4> */}
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
